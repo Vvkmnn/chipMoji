124aca614c6956c35947535f0dd9333627d851a0
Vivek Menon <mail@vvkmnn.xyz>
2017-06-14 18:07:32 -0700

    Tweaks.

--- js/slick.js
+++ js/slick.js
@@ -1,38 +1,36 @@
-(function() {
-  
+(function () {
+
   var gradients = ["linear-gradient(to bottom, red 0%, orange 100%) fixed",
-                   "linear-gradient(to bottom, yellow 0%, white 100%) fixed",
-                   "linear-gradient(to bottom, green 0%, black 100%) fixed",
-                   "linear-gradient(to bottom, white 0%, blue 100%) fixed",
-                   "linear-gradient(to bottom, red 0%, black 100%) fixed",
-                   "linear-gradient(to bottom, black 0%, blue 100%) fixed",
-                   "linear-gradient(to bottom, orange 0%, green 100%) fixed",
-                   "linear-gradient(to bottom, gray 0%, purple 100%) fixed",
-                   "linear-gradient(to bottom, black 0%, green 100%) fixed",
-                  "linear-gradient(to bottom, orange 0%, green 100%) fixed",
-                   "linear-gradient(to bottom, white 0%, blue 100%) fixed",
-                   ];
+    "linear-gradient(to bottom, green 0%, black 100%) fixed",
+    "linear-gradient(to bottom, yellow 0%, orange 100%) fixed",
+    "linear-gradient(to bottom, white 0%, blue 100%) fixed",
+    "linear-gradient(to bottom, darkred 0%, red 100%) fixed"
+  ];
 
   var currentIndex = 0;
 
 
   var slideContainer = $('.slide-container');
-  
-  slideContainer.slick();
-  
+
+  slideContainer.slick({
+    autoplay: true,
+    autoplaySpeed: 1200,
+
+  });
+
   $('.clash-card__image img').hide();
   $('.slick-active').find('.clash-card img').fadeIn(200);
-  
+
   // On before slide change
-  slideContainer.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+  slideContainer.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
     $('.slick-active').find('.clash-card img').fadeOut(1000);
 
   });
-  
+
   // On after slide change
-  slideContainer.on('afterChange', function(event, slick, currentSlide) {
+  slideContainer.on('afterChange', function (event, slick, currentSlide) {
     $('.slick-active').find('.clash-card img').fadeIn(200);
-    $("body").css("background", gradients[currentIndex++%gradients.length]);   
+    $("body").css("background", gradients[currentIndex++ % gradients.length]);
   });
-  
+
 })();
